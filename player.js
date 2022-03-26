170d627454fd4b0f732f5936aae736a6e18496f2
Daniel Abayev <danieldaniel151@gmail.com>
2022-03-26 18:43:28 +0300

    added initial actions to buttons

--- player.js
+++ player.js
@@ -1,9 +1,14 @@
 const GLS_JSON_URL = "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=?";
+
+/* Globals for JSON data */
 let data;
 let steps;
 let tipHtml;
 let hoverTipHtml;
 let tipCss;
+
+/* global for Tip state*/
+let currStepIdx = 0;
 
 function addJquery() {
     var script = document.createElement('script');
@@ -31,6 +36,40 @@ function fetchDataFromJson() {
     });
 }
 
+
+
+function onNextClick() {
+    let nextStepId = steps[currStepIdx].followers[0].next;
+    currStepIdx = steps.findIndex(value => value.id === nextStepId);
+    if (currStepIdx > 0) {
+        $("button[data-iridize-role='prevBt']").removeClass('default-prev-btn');
+    }
+    if (currStepIdx === steps.length - 2) {
+        $("a[data-iridize-role='nextBt']").css({display: 'none'})
+    }
+}
+
+function onPrevClick() {
+    currStepIdx--;
+    if (currStepIdx === 0) {
+        $("button[data-iridize-role='prevBt']").addClass('default-prev-btn');
+    }
+    if (currStepIdx < steps.length - 1) {
+        $("a[data-iridize-role='nextBt']").css({display: 'block'})
+    }
+}
+
+function onCloseClick() {
+    $('.sttip').css({display: 'none'});
+}
+
+function bindActionToButtons() {
+    $("a[data-iridize-role='nextBt']").click(onNextClick);
+    $("button[data-iridize-role='prevBt']").click(onPrevClick);
+    $("button[data-iridize-role='closeBt']").click(onCloseClick);
+
+}
+
 function addTipObject() {
     $("head").append("<style>" + tipCss + "</style>");
     $("body").append(
@@ -43,12 +82,15 @@ function addTipObject() {
         "</div>" +
         "</div>" +
         "</div>");
+
+    $("span[data-iridize-role='stepsCount']").text(steps.length - 1);
+
+    bindActionToButtons();
 }
 
 function loadGls() {
     addMainCss();
     fetchDataFromJson();
-    alert("yay");
 }
 
 addJquery();
